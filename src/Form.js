1041ba7b3bcdb8d6a9ad5cc6f093baf24f72c342
Gabriel Lima <gabriel.lima13372@gmail.com>
2018-12-04 15:16:53 -0200

    adicionando bootstrap 4 e estilizando o form

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import reactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 //import Contact from './models/contact';
-
-
 export default class Form extends React.Component{
 	constructor(props){
 		super(props);
@@ -16,25 +15,55 @@ export default class Form extends React.Component{
 	}
 
 	render(){
+		const btnMargin={
+			marginRight: '10px'
+		}
 		return(
-			<form >
-				<label htmlFor="name">Nome</label>
-				<input type="text" name="name" id="name" onChange=""/>
-				<label htmlFor="gender">Gênero</label>
-				<select type="text" name="gender" id="gender" onChange="">
-					<option defaultValue value="">Escolha</option>
-					<option value="MALE">Masculino</option>
-					<option value="FEMALE">Feminino</option>
-				</select>
-				<label htmlFor="date">Data</label>
-				<input type="date" name="date" id="date" onChange=""/>
-				<label htmlFor="email">E-mail</label>
-				<input type="email" name="email" id="email" onChange=""/>
-				<label htmlFor="phone">Telefone</label>
-				<input type="number" name="phone" id="phone" onChange=""/>
-				<button type="submit">Salvar</button>
-			</form>
-
+			<section className="container text-left">
+				<form>
+					<div className="form-row">
+						<div className="form-group col-sm-6">
+							<label htmlFor="name">Nome</label>
+							<input type="text" className="form-control" name="name" 
+							id="name"  
+							placeholder="Nome completo"/>
+						</div>
+						<div className="form-group col-sm-3">
+							<label htmlFor="gender">Gênero</label>
+							<select id="gender" name="gender"  
+							className="form-control" required >
+								<option defaultValue value="">Escolha</option>
+								<option value="MALE">Masculino</option>
+								<option value="FEMALE">Feminino</option>
+							</select>
+						</div>
+						<div className="form-group col-sm-3 ">
+							<label htmlFor="birthday">Nascimento</label>
+							<input type="date" className="form-control" name="birthday" 
+							 id="birthday"
+							placeholder="Data de Aniversário" />
+						</div>
+					</div>
+					<div className="form-row">
+						<div className="form-group col-sm-8">
+							<label htmlFor="email">Email</label>
+							<input type="email" name="email"  
+							className="form-control" id="email" 
+							placeholder="E-mail"/>
+						</div>
+						<div className="form-group col-sm-4">
+							<label htmlFor="phone">Telefone</label>
+							<input type="number" className="form-control" name="phone" 
+							 id="phone"
+							placeholder="Telefone" />
+						</div>
+					</div>
+					<div className="text-right">
+						<button style={btnMargin} className="btn btn-light">Voltar</button>
+						<button type="submit" className="btn btn-primary">Salvar</button>
+					</div>
+				</form>
+			</section>
 		)
 	}
 }
