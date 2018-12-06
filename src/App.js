a9de2626453ec4cada63efafab63ab8cc90b580a
Igor Modesto <igormodesto@Mac-Pro.local>
2018-12-06 14:09:29 -0200

    Página de Login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import 'antd/dist/antd.css';
 import './css/App.css';
+import {
+	Form, 
+	Icon, 
+	Input, 
+	Button, 
+	Checkbox
+  } from 'antd';
+import FormItem from 'antd/lib/form/FormItem';
 
 class App extends Component {
 	
 	render() {
 		return (
-			<div>
-				<section>
-					<Link to="/contacts">login</Link>
-				</section>
+			<div className="main">
+				<Form className="login-form">
+					<FormItem>
+						<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Usuário" />
+					</FormItem>
+					<FormItem>
+						<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Senha" />
+					</FormItem>
+					<FormItem>
+						<Checkbox>Remember me</Checkbox>
+						<a className="login-form-forgot" href="">Forgot password</a>
+					</FormItem>
+					<Button type="primary" className="login-form-button"> 
+						<Link to="/contacts">ENTRAR</Link>
+					</Button>
+				</Form>
 			</div>
 		);
 	}
