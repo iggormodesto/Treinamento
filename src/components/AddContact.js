a6b84b541a9f513c9d8d5feee2d293d3c068736b
Igor Modesto <igormodesto@Mac-Pro.local>
2018-12-06 15:54:45 -0200

    body da modal

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -68,45 +68,44 @@ export default class AddContact extends React.Component{
 			>
 				<section className="container text-left">
 					<form>
-						<div className="form-group col-sm-6">
-							<label htmlFor="name">Nome</label>
+						<div className="form-group">
+							<label htmlFor="name">Nome:</label>
 							<input type="text" className="form-control" name="name" id="name"
 							value={this.state.contact.name || ''}
 							onChange={this.handleInputChange} 
 							placeholder="Nome completo"/>
 						</div>
-						<div className="form-group col-sm-3">
-							<label htmlFor="gender">Gênero</label>
+						<div className="form-group">
+							<label htmlFor="gender">Gênero:</label>
 							<select id="gender" name="gender" className="form-control" required
 							value={this.state.contact.gender || ''}
 							onChange={this.handleInputChange}>
-								<option defaultValue value="">Escolha</option>
+								<option defaultValue value="">Selecione</option>
 								<option value="MALE">Masculino</option>
 								<option value="FEMALE">Feminino</option>
 							</select>
 						</div>
-						<div className="form-group col-sm-3 ">
-							<label htmlFor="birthday">Nascimento</label>
+						<div className="form-group">
+							<label htmlFor="birthday">Nascimento:</label>
 							<input type="date" className="form-control" name="birthday" id="birthday"
 							value={this.state.contact.birthday || ''}
-							onChange={this.handleInputChange}
-							placeholder="Data de Aniversário" />
+							onChange={this.handleInputChange} />
 						</div>
-						<div className="form-group col-sm-8">
-							<label htmlFor="email">Email</label>
+						<div className="form-group">
+							<label htmlFor="email">Email:</label>
 							<input type="email" name="email"  
 							className="form-control" id="email" 
 							value={this.state.contact.email || ''}
 							onChange={this.handleInputChange}
-							placeholder="E-mail"/>
+							placeholder="usuario@dominio.com.br"/>
 						</div>
-						<div className="form-group col-sm-4">
-							<label htmlFor="phone">Telefone</label>
+						<div className="form-group">
+							<label htmlFor="phone">Telefone:</label>
 							<input type="number" className="form-control"
 							name="phone" id="phone"
 							value={this.state.contact.phone || ''}
 							onChange={this.handleInputChange}
-							placeholder="Telefone" />
+							placeholder="(00) 00000-0000" />
 						</div>
 					</form>
 				</section>
